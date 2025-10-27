f5e4395c5e89ecc1fae70227cc5e0a23905db40e
Abhishek Srivastav <53862677+Abhishek-IOT@users.noreply.github.com>
2025-10-27 16:21:13 +0530

    Update my-glue-job.py

--- my-glue-job.py
+++ my-glue-job.py
@@ -1,8 +1,10 @@
 import sys
+from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
+from awsgluedq.transforms import EvaluateDataQuality
 
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
 sc = SparkContext()
@@ -11,14 +13,18 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
-# ✅ Read CSV files from input folder
-df = spark.read.option("header", "true").csv("s3://aws-glue-assets-640958509818-us-east-1/input/")
+# Default ruleset used by all target nodes with data quality enabled
+DEFAULT_DATA_QUALITY_RULESET = """
+    Rules = [
+        ColumnCount > 0
+    ]
+"""
 
-# ✅ Example transformation: select only id and name
-df = df.select("id", "name")
+# Script generated for node Amazon S3
+AmazonS3_node1759213108259 = glueContext.create_dynamic_frame.from_options(format_options={"multiLine": "false"}, connection_type="s3", format="json", connection_options={"paths": ["s3://ridestream-production/2025/"], "recurse": True}, transformation_ctx="AmazonS3_node1759213108259")
 
-# ✅ Write to processed/output folder as parquet
-df.write.mode("overwrite").parquet("s3://aws-glue-assets-640958509818-us-east-1/output/")
-print("Job has been executed Successfully")
+# Script generated for node Amazon S3
+EvaluateDataQuality().process_rows(frame=AmazonS3_node1759213108259, ruleset=DEFAULT_DATA_QUALITY_RULESET, publishing_options={"dataQualityEvaluationContext": "EvaluateDataQuality_node1759211519266", "enableDataQualityResultsPublishing": True}, additional_options={"dataQualityResultsPublishing.strategy": "BEST_EFFORT", "observations.scope": "ALL"})
+AmazonS3_node1759213798527 = glueContext.write_dynamic_frame.from_options(frame=AmazonS3_node1759213108259, connection_type="s3", format="glueparquet", connection_options={"path": "s3://aws-glue-assets-640958509818-us-east-1/dags/", "partitionKeys": []}, format_options={"compression": "snappy"}, transformation_ctx="AmazonS3_node1759213798527")
+
 job.commit()
-
